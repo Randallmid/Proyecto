1b4872f7856d45ce9b1da5bbc8608cf342d43e75
Randall <168565852+Randallmid@users.noreply.github.com>
2024-09-10 10:52:39 -0600

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,16 @@
+ class Examen {
+     val rc: CharArray= charArrayOf('a','c','b','a','d','b','b','c','a','a','b','d')
+     val nombres = arrayOf("marta","Pedro","juan","maria")
+     val notas: FloatArray = floatArrayOf(100f,58.33f,66.66f,83.33f)
+     var matrizxd = arrayOf(
+         charArrayOf('a', 'c', 'b', 'a', 'd', 'b', 'b', 'c', 'a', 'a', 'b', 'd'),
+         charArrayOf('b', 'c', 'b', 'd', 'd', 'b', 'b', 'a', 'b', 'd', 'b', 'd'),
+         charArrayOf('a', 'c', 'b' , 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'b', 'c'),
+         charArrayOf('a', 'c', 'b', 'a', 'd', 'b', 'b', 'c', 'a', 'a', 'b', 'c')
+     )
+//OHH SI FREDDY
+
+ }
+
+fun main() {
+
